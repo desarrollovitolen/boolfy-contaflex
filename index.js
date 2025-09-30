9728e01f892a9e77af8bad2f0a8376eabd9127f0
César Elias Castillo <picocastillo@Cesars-MacBook-Air.local>
2025-09-30 16:00:14 -0300

    Added credentials to contaflex

--- index.js
+++ index.js
@@ -6,62 +6,61 @@ const multer = require('multer');
 const csv = require('csv-parser');
 
 const app = express();
-const port = 3000;
+const port = 443;
 
 // Configuración de multer para manejar la carga de archivos CSV
 const upload = multer({ dest: 'uploads/' });
     
 // Verifica si la cabecera es correcta
 const validateHeaders = (req, res, next) => {
-  const headerValue = req.headers['asdasdasd'];
-  if (headerValue && headerValue === 'asdasdasd') {
-    next(); // La cabecera es correcta, continúa al siguiente middleware
-  } else {
-    res.status(400).json({ error: 'Cabecera inválida' });
-  }
+    const headerValue = req.headers['SDm^rJ!1CI*NFDpO^4V*V2y96ujBwXDtK1pmyxpA2b'];
+    if (headerValue && headerValue === 'rfCXxVU52RTnqBl4ggvtWmaCbYms&i%D@8napV5QTu') {
+        next(); 
+    } else {
+        res.status(400).json({ error: 'Cabecera inválida' });
+    }
 };
 
-// Endpoint para recibir y procesar el archivo CSV
+
 app.post('/upload-csv', validateHeaders, upload.single('csvFile'), (req, res) => {
-  if (!req.file) {
-    return res.status(400).json({ error: 'No se ha cargado ningún archivo CSV' });
-  }
+    if (!req.file) {
+        return res.status(400).json({ error: 'No se ha cargado ningún archivo CSV' });
+    }
 
-  const filePath = path.join(__dirname, req.file.path);
+    const filePath = path.join(__dirname, req.file.path);
 
-  const results = [];
-  fs.createReadStream(filePath)
-    .pipe(csv())
-    .on('data', (data) => results.push(data))
-    .on('end', () => {
-      // Guardamos el archivo CSV en una ubicación específica en el servidor
-      const outputFilePath = path.join(__dirname, 'output', 'result.csv');
-      const outputStream = fs.createWriteStream(outputFilePath);
-      results.forEach((row) => {
-        outputStream.write(Object.values(row).join(',') + '\n');
-      });
-      outputStream.end();
+    const results = [];
+    fs.createReadStream(filePath)
+    .pipe(csv())
+    .on('data', (data) => results.push(data))
+    .on('end', () => {
+    const outputFilePath = path.join(__dirname, 'output', 'result.csv');
+    const outputStream = fs.createWriteStream(outputFilePath);
+    results.forEach((row) => {
+    outputStream.write(Object.values(row).join(',') + '\n');
+    });
+    outputStream.end();
 
-      // Ahora enviamos el archivo CSV por FTP (o cualquier protocolo que desees)
-      const targetUrl = 'ftp://direccion:puerto/carpeta_destino/result.csv';  // Cambia esto a la URL deseada
-      axios
-        .put(targetUrl, fs.createReadStream(outputFilePath), {
-          headers: {
-            'Content-Type': 'application/octet-stream',
-          },
-        })
-        .then((response) => {
-          console.log('Archivo enviado exitosamente');
-          res.status(200).json({ message: 'Archivo procesado y enviado exitosamente' });
-        })
-        .catch((error) => {
-          console.error('Error al enviar el archivo:', error);
-          res.status(500).json({ error: 'Error al enviar el archivo' });
-        });
-    });
+    // Ahora enviamos el archivo CSV por FTP (o cualquier protocolo que desees)
+    const targetUrl = 'ftp://vitoweb:ps5ty15se36@132.132.132.132/home/jorge/files_to_test_boolfy/result.csv';  // Cambia esto a la URL deseada
+    axios
+    .put(targetUrl, fs.createReadStream(outputFilePath), {
+    headers: {
+    'Content-Type': 'application/octet-stream',
+    },
+    })
+    .then((response) => {
+    console.log('Archivo enviado exitosamente');
+    res.status(200).json({ message: 'Archivo procesado y enviado exitosamente' });
+    })
+    .catch((error) => {
+    console.error('Error al enviar el archivo:', error);
+    res.status(500).json({ error: 'Error al enviar el archivo' });
+    });
+    });
 });
 
 // Levantar el servidor en el puerto indicado
 app.listen(port, () => {
-  console.log(`Servidor escuchando en http://localhost:${port}`);
+    console.log(`Servidor escuchando en http://localhost:${port}`);
 });
